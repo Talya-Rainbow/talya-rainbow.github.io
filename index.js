daf0e0435b8a393bfab950ae20fcce535318d3e1
Talya-Rainbow <talya.rainbow@gmail.com>
2025-12-14 18:01:09 +0200

    comit

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ let secret = document.getElementsByClassName('secret-link')[0];
 secret.addEventListener('mouseenter',function() {
     secret.style.top = Math.random() * 100 + 'em';
 });
+
+
+let stylish = document.getElementById("stylish");
+console.log(stylish);
+stylish.addEventListener("click", function() {
+    console.log("click")
+stylish.style.backgroundColor="rgba(143, 68, 162, 1)"
+});
